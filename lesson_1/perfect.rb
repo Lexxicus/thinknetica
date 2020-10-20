b8f4093fa045ebb55a9059fe3c5d1bfdd00a974b
Lexxicus <alexxxicus@gmail.com>
2020-10-20 20:00:43 +0500

    pravki

--- lesson_1/perfect.rb
+++ lesson_1/perfect.rb
@@ -3,6 +3,6 @@ name = gets.chomp.capitalize
 print 'Укажите ваш рост: '
 height = gets.to_f
 # расчёт идеального веса
-p_weight = (height - 110) * 1.15
+p_weight = (height - 110)*1.15
 
 print p_weight > 0 ? "#{name} your perfect weight is : #{p_weight}" : "#{name} your weight is perfect"
